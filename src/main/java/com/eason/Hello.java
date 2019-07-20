eb9b6e2c6742df9b681440fee6bfdc83887df9f2
warn0972 <warn.errorpk@gmail.com>
2019-07-21 04:07:49 +0800

    Java ane Kotlin data type

--- src/main/java/com/eason/Hello.java
+++ src/main/java/com/eason/Hello.java
@@ -6,6 +6,24 @@ public class Hello {
 //        new Proson().hello();
         Proson p = new Proson();
         p.hello();
+        int i = 1;
+        Integer i2 = 1; //包裝類變為基本資料型態所設計
+
+        long l = 3;
+        short s = 4;
+        float f = 1.2f;
+        double d = 4.34;
+        char c = 'A';
+        Character c2 = 'A';
+        byte b = 120;
+
+        boolean adult = true;
+        boolean enroll = false;
+
+        //Java裡面的基本資料型態其變數的型態名稱全都是小寫
+        String name= "Eason";
+        //參照資料型態特點型態名稱第一個字是大寫
+
     }
 
 }
--- src/main/kotlin/com/eason/Hello.kt
+++ src/main/kotlin/com/eason/Hello.kt
@@ -8,6 +8,14 @@ fun main(args: Array<String>) {
     Human().hello()
     val h = Human()
     h.hello()
+    //val給值後不可改變var可以
+    val age : Int = 19    //沒有特別指定資料型態，若要指定資料型態可在變數後面加上 :資料型態
+    var weight = 77.3
+    var weight2 = 44.3f   //轉成float
+    val name = "Eason"
+    var name2 : String
+    name2 = "Eason"
+
 }
 class Human {
     fun hello(){
